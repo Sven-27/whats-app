12c9811c49de29818f089df19dc9b6b8c29f78ac
Sven Notermans <sven.notermans@live.nl>
2021-03-02 22:05:41 +0100

    working on sidebarchat

---
+++ src/components/Sidebar.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import './Sidebar.css'
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import ChatIcon from '@material-ui/icons/Chat';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import {Avatar, IconButton} from '@material-ui/core';
+import SearchOutlinedIcon from '@material-ui/icons/SearchOutlined';
+import SidebarChat from './SidebarChat'
+
+const Sidebar = () => {
+    return (
+        <div className="sidebar">
+            <div className="sidebar__header">
+                <Avatar />
+                <div className="sidebar__header__right">
+                    <IconButton>
+                <DonutLargeIcon />
+                </IconButton>
+                <IconButton>
+                <ChatIcon />
+                </IconButton>
+                <IconButton>
+                <MoreVertIcon />
+                </IconButton>
+                </div>
+            </div>
+            <div className="sidebar__search">
+                <div className="sidebar__searchContainer">
+                    <SearchOutlinedIcon />
+                    <input type="text" placeholder="Search or start a new chat" />
+                </div>
+            </div>
+            <div className="sidebar__chats">
+                <SidebarChat />
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar
+
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC-vZ6-Q8UcJOlt6RWDHFo2g_97w_ph_-M",
+    authDomain: "whats-app-81a1f.firebaseapp.com",
+    projectId: "whats-app-81a1f",
+    storageBucket: "whats-app-81a1f.appspot.com",
+    messagingSenderId: "895505017936",
+    appId: "1:895505017936:web:8767523b4efa0578463555"
+  };
